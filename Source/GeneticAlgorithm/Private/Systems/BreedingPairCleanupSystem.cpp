e248012b0a7428ce21f02ad6e0dc34e683971a61
renatok <kuurstra.renato@gmail.com>
2025-10-30 15:07:11 +0000

    Cleanup system to run at the end of the breeding phase of the genetic algorithm.

---
+++ Source/GeneticAlgorithm/Private/Systems/BreedingPairCleanupSystem.cpp
@@ -0,0 +1,17 @@
+﻿#include "Systems/BreedingPairCleanupSystem.h"
+#include "Components/BreedingPairComponent.h"
+
+void UBreedingPairCleanupSystem::Update(float /*DeltaTime*/)
+{
+    auto& Registry = GetRegistry();
+
+    // Destroy all entities that carry FBreedingPairComponent
+    auto View = GetView<FBreedingPairComponent>();
+
+    // Collect first to avoid iterator invalidation concerns
+    TArray<entt::entity> ToDestroy;
+    for (auto Entity : View)
+    {
+        Registry.destroy(Entity);
+    }
+}
---
+++ Source/GeneticAlgorithm/Public/Systems/BreedingPairCleanupSystem.h
@@ -0,0 +1,24 @@
+﻿//Copyright (c) 2025 Renato Kuurstra. Licensed under the MIT License. See LICENSE file in the project root for details.
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EcsSystem.h"
+#include "Components/BreedingPairComponent.h"
+#include "BreedingPairCleanupSystem.generated.h"
+
+/**
+ * Very simple system: destroys all entities that carry FBreedingPairComponent.
+ * Why: Clean up transient pairing/link entities after they have been consumed.
+ */
+UCLASS()
+class GENETICALGORITHM_API UBreedingPairCleanupSystem : public UEcsSystem
+{
+	GENERATED_BODY()
+public:
+	UBreedingPairCleanupSystem()
+	{
+		RegisterComponent<FBreedingPairComponent>();
+	}
+
+	virtual void Update(float DeltaTime = 0.0f) override;
+};
