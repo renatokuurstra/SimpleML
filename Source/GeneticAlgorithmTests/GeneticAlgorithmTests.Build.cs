317a671cc6141e816827ff26caa2603d0a683460
renatok <kuurstra.renato@gmail.com>
2025-10-31 10:47:08 +0000

    Not immediately useful, but I did add a module for tests related to the genetic algorithm

---
+++ Source/GeneticAlgorithmTests/GeneticAlgorithmTests.Build.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) 2025 Renato Kuurstra. Licensed under the MIT License. See LICENSE file in the project root for details.
+
+using UnrealBuildTool;
+
+public class GeneticAlgorithmTests : ModuleRules
+{
+    public GeneticAlgorithmTests(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(new string[]
+        {
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "UEcs",
+            "GeneticAlgorithm"
+        });
+
+        PrivateDependencyModuleNames.AddRange(new string[]
+        {
+        });
+    }
+}
---
+++ Source/GeneticAlgorithmTests/Private/GeneticAlgorithmTestsModule.cpp
@@ -0,0 +1,12 @@
+﻿// Copyright (c) 2025 Renato Kuurstra. Licensed under the MIT License. See LICENSE file in the project root for details.
+
+#include "Modules/ModuleManager.h"
+
+class FGeneticAlgorithmTestsModule : public IModuleInterface
+{
+public:
+	virtual void StartupModule() override {}
+	virtual void ShutdownModule() override {}
+};
+
+IMPLEMENT_MODULE(FGeneticAlgorithmTestsModule, GeneticAlgorithmTests)
